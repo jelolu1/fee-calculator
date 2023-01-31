f1192109fb1611acf6622679f2c308e05e75ce94
jelolu1 <jelolu1@gmail.com>
2023-01-31 13:17:17 +0000

    m

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,53 @@
-import styles from './App.module.css'
-import { CalcField } from './components/calc/CalcField'
+import { useEffect, useState } from 'react';
+import styles from './App.module.css';
+import { CalcField } from './components/calc/CalcField';
 
-// https://github.com/woltapp/engineering-summer-intern-2023
+// https://github.com/woltap33p/engineering-summer-intern-2023
+
+type calculateFeeType = {
+  cartValue: number,
+  deliveryDistance: number,
+  numberOfItems: number,
+  orderTime: string
+}
+
 
 function App() {
+
+  const [cartValue, setCartValue] = useState<number>(0);
+  const [deliveryDistance, setDeliveryDistance] = useState<number>(0);
+  const [numberOfItems, setNumberOfItems] = useState<number>(0);
+  const [orderTime, setOrderTime] = useState<string>("");
+  const [btnOn, setBtnOn] = useState<boolean>(false);
+
+
+
+  const calculateFee = (cartValue: number, deliveryDistance: number, numberOfItems: number, orderTime: string) =>{
+    
+  }
+
+  const submitClickHandler = (e: any) => {
+    e.preventDefault();
+
+    // Indicate user that needs to fill fields
+    if (!btnOn) return;
+
+    calculateFee(cartValue, deliveryDistance, numberOfItems, orderTime)
+
+  }
+
+  useEffect(() => {
+    if(
+      cartValue !== undefined &&
+      deliveryDistance !== undefined &&
+      numberOfItems !== undefined &&
+      orderTime !== undefined
+    ) setBtnOn(true)
+    else setBtnOn(false)
+
+
+  }, [cartValue, deliveryDistance, numberOfItems, orderTime])
+
 
   return (
 
@@ -11,16 +55,42 @@ function App() {
       <header className={styles['calc-header']}>
       </header>
       <form className={styles['calc-form']}>
-        <CalcField title='Cart Value' inputType='number' unit='€' />
-        <CalcField title='Delivery Distance' inputType='number' unit='Meters' />
-        <CalcField title='Amount of Items' inputType='number' unit='Items' />
-        <CalcField title='Time' inputType='date' unit={null} />
+        <CalcField
+          title='Cart Value'
+          inputType='number'
+          unit='€'
+          setValue={setCartValue}
+        />
+        <CalcField
+          title='Delivery Distance'
+          inputType='number'
+          unit='Meters'
+          setValue={setDeliveryDistance}
+        />
+        <CalcField
+          title='Amount of Items'
+          inputType='number'
+          unit='Items'
+          setValue={setNumberOfItems}
+        />
+        <CalcField
+          title='Time'
+          inputType='datetime-local'
+          unit={null}
+          setValue={setOrderTime}
+        />
 
-        
-  
+
+        <button
+          onClick={submitClickHandler} 
+          className={`${styles['calc-button']} ${btnOn ? styles['btn-on']: styles['btn-off']}`}
+        >
+          Calculate Fee
+        </button>
+
+        <p>Delivery Fee: </p>
       </form>
       <footer  className={styles['calc-footer']}>
-        
       </footer>
       
     </main>
--- src/components/calc/CalcField.tsx
+++ src/components/calc/CalcField.tsx
@@ -1,20 +1,39 @@
+import { useRef } from 'react';
 import styles from './calcField.module.css';
 
 type CalcFieldProps = {
     title: string,
     inputType: string,
-    unit: string | null
+    unit: string | null,
+    setValue: Function        
+
 }
+
 
 export const CalcField = (props: CalcFieldProps) => {
 
+
     return (
-        <div className={styles['calc-field']}>
+        <div className={`${styles['calc-field']} ${props.inputType === "datetime-local" ? styles['calc-field-date'] : ""}`}>
             <p>{props.title}</p>
             {
                 props.inputType==="number" 
-                ? <input className={styles['input-value']} type={props.inputType} min="0"></input>
-                : <input className={`${styles['input-value']} ${styles['input-calendar']}`} type={props.inputType} ></input>
+                && <input
+                    className={styles['input-value']}
+                    type={props.inputType}
+                    min="0"
+                    onChange={(e) => props.setValue(e.target.value)}
+                />
+
+            }
+            {
+                props.inputType==="datetime-local" 
+                && <input
+                    className={`${styles['input-value']} ${styles['input-date']}`}
+                    type={props.inputType}
+                    onChange={(e) => props.setValue(e.target.value)
+                    }
+                />
             }
             {
                 props.unit 
